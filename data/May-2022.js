eede8a1023f38f87708fddc5fb859340450cba41
sinkinben <sinkinben@outlook.com>
2022-05-29 20:23:57 +0800

    feat: init index.html and css

---
+++ data/May-2022.js
@@ -0,0 +1,37 @@
+var publishedItems = [
+  {
+    "date": "2022/05/30",
+    "type": "AppleMusic",
+    "title": "陈奕迅 -《阿猫阿狗》",
+    "lyric": [
+      "大人物最懂开玩笑",
+      "谁都很重要",
+      "又被谁忘掉"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E9%98%BF%E8%B2%93%E9%98%BF%E7%8B%97/1386714588?i=1386714604&l=en"
+  },
+  {
+    "date": "2022/05/29",
+    "redirect": "https://movie.douban.com/subject/1889243/",
+    "title": "《星际穿越》",
+    "type": "Article Cover",
+    "context": [
+      "Do not go gentle into that good night, ",
+      "Old age should burn and rave at close of day,",
+      "Rage, rage against the dying of the light.",
+    ],
+    "backgroundImage": "images/xi-ji-chuan-yue.png"
+  },
+  {
+    "date": "2022/05/29",
+    "type": "AppleMusic",
+    "title": "谢安琪 -《年度之歌》",
+    "lyric": [
+      "还愿「你」懂下台的美丽",
+      "鞠躬了就退位",
+      "起码得到敬礼",
+      "谁又妄想一曲一世让人忠心到底"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E5%B9%B4%E5%BA%A6%E4%B9%8B%E6%AD%8C/1443335510?i=1443335836&l=en"
+  }
+]
