1476c099ec697b1adc74f0a24927f5767f645967
sinkinben <sinkinben@outlook.com>
2022-06-12 18:04:25 +0800

    feat: publish new item

--- data/June-2022.js
+++ data/June-2022.js
@@ -1,5 +1,19 @@
 var June2022 = [
   {
+    "date": "2022/06/12",
+    "type": "AppleMusic",
+    "title": "王菲 -《开到荼蘼》",
+    "lyric": [
+      "谁曾伤天害理 谁又是上帝",
+      "我们在等待 什么奇迹",
+      "最后剩下自己 舍不得挑剔",
+      "最后对着自己 也不大看得起",
+      "谁给我全世界 我都会怀疑",
+      "心花怒放 却开到荼蘼"
+    ],
+    "embedLink": "https://embed.music.apple.com/cn/album/%E9%96%8B%E5%88%B0%E8%8D%BC%E8%98%BC-live/966486703?i=966486710&l=en"
+  },
+  {
     "date": "2022/06/10",
     "title": "by sinkinben",
     "type": "Article Cover",
