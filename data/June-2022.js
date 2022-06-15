0cdd48e2b371648dcdc08ad31e3ca1ea641639ac
sinkinben <sinkinben@outlook.com>
2022-06-15 23:21:38 +0800

    feat: publish new item

--- data/June-2022.js
+++ data/June-2022.js
@@ -1,5 +1,19 @@
 var June2022 = [
   {
+    "date": "2022/06/15",
+    "type": "AppleMusic",
+    "title": "陳奕迅 -《黑暗中漫舞》",
+    "lyric": [
+      "為何未曾讓我得夠便要損失你",
+      "捱到那一天參透佛理",
+      "手會鬆燈會滅有限期",
+      "為何未能讓我死去便要認識你",
+      "難道你很想天使問我",
+      "我的舞步跳得可美"
+    ],
+    "embedLink": "https://embed.music.apple.com/cn/album/%E9%BB%91%E6%9A%97%E4%B8%AD%E6%BC%AB%E8%88%9E/542593700?i=542593736&l=en"
+  },
+  {
     "date": "2022/06/12",
     "type": "AppleMusic",
     "title": "王菲 -《开到荼蘼》",
