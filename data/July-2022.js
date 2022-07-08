a58779e3e82b698bacd2a4746b8292448a6feb1f
sinkinben <sinkinben@outlook.com>
2022-07-08 22:04:33 +0800

    feat: share apple music item
    Signed-off-by: sinkinben <sinkinben@outlook.com>

---
+++ data/July-2022.js
@@ -0,0 +1,13 @@
+var July2022 = [
+  {
+    "date": "2022/07/08",
+    "type": "AppleMusic",
+    "title": "王菲 -《曖昧》",
+    "lyric": [
+      "猶疑在似即若離之間 望不穿這曖昧的眼",
+      "似是濃卻仍然很淡",
+      "天早灰藍 想告別 偏未晚"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E6%9B%96%E6%98%A7/1443424435?i=1443425251&l=en"
+  },
+]
