0f229100f45f849365868e4b079a48c03d47e8a5
sinkinben <sinkinben@outlook.com>
2022-07-09 15:14:57 +0800

    feat: shared apple music
    Signed-off-by: sinkinben <sinkinben@outlook.com>

--- data/July-2022.js
+++ data/July-2022.js
@@ -1,5 +1,20 @@
 var July2022 = [
   {
+    "date": "2022/07/09",
+    "type": "AppleMusic",
+    "title": "王菲 -《開到荼蘼》",
+    "lyric": [
+      "每一个人",
+      "伤心了就哭泣",
+      "饿了就要吃",
+      "相差大不过天地 有何刺激",
+      "有太多太多魔力 太少道理",
+      "太多太多游戏 只是为了好奇",
+      "还有什么值得 歇斯底里"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E9%96%8B%E5%88%B0%E8%8D%BC%E8%98%BC-live/966486703?i=966486710&l=en"
+  },
+  {
     "date": "2022/07/08",
     "type": "AppleMusic",
     "title": "王菲 -《曖昧》",
