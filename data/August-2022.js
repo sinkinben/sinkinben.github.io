55be6d54f6ec445709610e825f726cae81951e1a
sinkinben <sinkinben@outlook.com>
2022-08-06 13:30:14 +0800

    shared apple music
    Signed-off-by: sinkinben <sinkinben@outlook.com>

---
+++ data/August-2022.js
@@ -0,0 +1,11 @@
+var August2022 = [
+  {
+    "date": "2022/08/06",
+    "type": "AppleMusic",
+    "title": "Chan Fai Young x Women's Choir -《暗涌》",
+    "lyric": [
+      "「越美丽的东西我越不可碰」"
+    ],
+    "embedLink": "https://embed.music.apple.com/cn/album/%E6%9A%97%E6%B9%A7/1485523497?i=1485523498&l=en"
+  },
+]
