5b7ae9625982c81babe9b8f862ae1899903674ec
sinkinben <sinkinben@outlook.com>
2022-08-07 23:21:30 +0800

    feat: share apple music
    Signed-off-by: sinkinben <sinkinben@outlook.com>

--- data/August-2022.js
+++ data/August-2022.js
@@ -1,5 +1,14 @@
 var August2022 = [
   {
+    "date": "2022/08/07",
+    "type": "AppleMusic",
+    "title": "Chan Fai Young x Women's Choir -《明日有明日》",
+    "lyric": [
+      "\"Tomorrow is another day.\""
+    ],
+    "embedLink": "https://embed.music.apple.com/cn/album/%E6%98%8E%E6%97%A5%E6%9C%89%E6%98%8E%E5%A4%A9/1468240101?i=1468240103&l=en"
+  },
+  {
     "date": "2022/08/06",
     "type": "AppleMusic",
     "title": "Chan Fai Young x Women's Choir -《暗涌》",
