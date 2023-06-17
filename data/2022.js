0dbd6fd6a695484993d21357853d0470016b7bd0
sinkinben <sinkinben@outlook.com>
2023-06-17 15:47:40 +0800

    share '邮差' by Faye Wong
    Signed-off-by: sinkinben <sinkinben@outlook.com>

--- data/2022.js
+++ data/2022.js
@@ -1,3 +1,88 @@
+var August2022 = [
+  {
+    "date": "2022/08/07",
+    "type": "AppleMusic",
+    "title": "Chan Fai Young x Women's Choir -《明日有明日》",
+    "lyric": [
+      "\"Tomorrow is another day.\""
+    ],
+    "embedLink": "https://embed.music.apple.com/cn/album/%E6%98%8E%E6%97%A5%E6%9C%89%E6%98%8E%E5%A4%A9/1468240101?i=1468240103&l=en"
+  },
+  {
+    "date": "2022/08/06",
+    "type": "AppleMusic",
+    "title": "Chan Fai Young x Women's Choir -《暗涌》",
+    "lyric": [
+      "「越美丽的东西我越不可碰」"
+    ],
+    "embedLink": "https://embed.music.apple.com/cn/album/%E6%9A%97%E6%B9%A7/1485523497?i=1485523498&l=en"
+  },
+];
+var July2022 = [
+  {
+    "date": "2022/07/31",
+    "type": "AppleMusic",
+    "title": "Chan Fai Young x Women's Choir -《火鳥》",
+    "lyric": [
+      "從頭細數命運由甚麼鑄成",
+      "心境每秒在註釋生命",
+      "密雲外還有清空",
+      "眼淚哪可遮掩眼睛"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E7%81%AB%E9%B3%A5/1491887280?i=1491887281&l=en"
+  },
+  {
+    "date": "2022/07/30",
+    "type": "AppleMusic",
+    "title": "Chan Fai Young x Women's Choir -《絕》",
+    "lyric": [
+      "讓痛苦",
+      "輪迴千次",
+      "彰顯那快樂有盡時"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E7%B5%95-%E4%BC%8A%E9%A4%A8%E7%8F%BE%E5%A0%B4%E7%89%88-live/1621840374?i=1621840375&l=en"
+  },
+  {
+    "date": "2022/07/29",
+    "type": "AppleMusic",
+    "title": "陈辉阳 -《日与夜》",
+    "lyric": [
+      "当你我错过今天",
+      "人潮将彼此盖掩",
+      "仍期望有生一天",
+      "跟你有缘吻一遍",
+      "与你约错终点",
+      "再邂逅是哪天"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E6%97%A5%E8%88%87%E5%A4%9C-%E9%BA%A5%E8%8A%B1%E8%87%A3%E7%89%88/1198129998?i=1198130197&l=en"
+  },
+  {
+    "date": "2022/07/09",
+    "type": "AppleMusic",
+    "title": "王菲 -《開到荼蘼》",
+    "lyric": [
+      "每一个人",
+      "伤心了就哭泣",
+      "饿了就要吃",
+      "相差大不过天地 有何刺激",
+      "有太多太多魔力 太少道理",
+      "太多太多游戏 只是为了好奇",
+      "还有什么值得 歇斯底里"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E9%96%8B%E5%88%B0%E8%8D%BC%E8%98%BC-live/966486703?i=966486710&l=en"
+  },
+  {
+    "date": "2022/07/08",
+    "type": "AppleMusic",
+    "title": "王菲 -《曖昧》",
+    "lyric": [
+      "猶疑在似即若離之間 望不穿這曖昧的眼",
+      "似是濃卻仍然很淡",
+      "天早灰藍 想告別 偏未晚"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E6%9B%96%E6%98%A7/1443424435?i=1443425251&l=en"
+  },
+];
 var June2022 = [
   {
     "date": "2022/06/25",
@@ -124,4 +209,40 @@ var June2022 = [
     ],
     "embedLink": "https://embed.music.apple.com/hk/album/%E5%A4%95%E9%99%BD%E4%B9%8B%E6%AD%8C/896631887?i=896631913&l=en"
   }
-]
+];
+var May2022 = [
+  {
+    "date": "2022/05/30",
+    "type": "AppleMusic",
+    "title": "陈奕迅 -《阿猫阿狗》",
+    "lyric": [
+      "大人物最懂开玩笑",
+      "谁都很重要",
+      "又被谁忘掉"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E9%98%BF%E8%B2%93%E9%98%BF%E7%8B%97/1386714588?i=1386714604&l=en"
+  },
+  {
+    "date": "2022/05/29",
+    "title": "《星际穿越》",
+    "type": "Sentences",
+    "context": [
+      "Do not go gentle into that good night, ",
+      "Old age should burn and rave at close of day,",
+      "Rage, rage against the dying of the light.",
+    ],
+    "backgroundImage": "images/xi-ji-chuan-yue.png"
+  },
+  {
+    "date": "2022/05/29",
+    "type": "AppleMusic",
+    "title": "谢安琪 -《年度之歌》",
+    "lyric": [
+      "还愿「你」懂下台的美丽",
+      "鞠躬了就退位",
+      "起码得到敬礼",
+      "谁又妄想一曲一世让人忠心到底"
+    ],
+    "embedLink": "https://embed.music.apple.com/hk/album/%E5%B9%B4%E5%BA%A6%E4%B9%8B%E6%AD%8C/1443335510?i=1443335836&l=en"
+  }
+];
---
+++ data/June-2023.js
@@ -0,0 +1,14 @@
+var June2023 = [
+    {
+        "date": "2023/06/17",
+        "type": "AppleMusic",
+        "title": "Faye Wong - 《邮差》",
+        "lyric": [
+            "你是一封信 我是邮差",
+            "最后一双脚 惹尽尘埃",
+            "忙着去护送 来不及拆开",
+            "里面完美的世界",
+        ],
+        "embedLink": "https://music.apple.com/cn/album/%E9%83%B5%E5%B7%AE-%E5%BB%A3%E6%9D%B1%E8%A9%B1/966481932?i=966481945&l=en"
+    },
+];
