21a3262df1f5f7e756631ac2bd8b3ec445869533
sinkinben <sinkinben@outlook.com>
2023-06-18 17:45:02 +0800

    Share '夕阳无限好' by Eason Chan
    Signed-off-by: sinkinben <sinkinben@outlook.com>

--- data/June-2023.js
+++ data/June-2023.js
@@ -1,14 +1,23 @@
 var June2023 = [
     {
+        "date": "2023/06/18",
+        "type": "AppleMusic",
+        "title": "陈奕迅 -《夕阳无限好》",
+        "lyric": [
+            "风花雪月不等人 要献便献吻"
+        ],
+        "embedLink": "https://embed.music.apple.com/hk/album/%E5%A4%95%E9%99%BD%E7%84%A1%E9%99%90%E5%A5%BD-live/1462045698?i=1462046112"
+    },
+    {
         "date": "2023/06/17",
         "type": "AppleMusic",
-        "title": "Faye Wong - 《邮差》",
+        "title": "Faye Wong -《邮差》",
         "lyric": [
             "你是一封信 我是邮差",
             "最后一双脚 惹尽尘埃",
             "忙着去护送 来不及拆开",
             "里面完美的世界",
         ],
-        "embedLink": "https://music.apple.com/cn/album/%E9%83%B5%E5%B7%AE-%E5%BB%A3%E6%9D%B1%E8%A9%B1/966481932?i=966481945&l=en"
-    },
+        "embedLink": "https://embed.music.apple.com/hk/album/%E9%83%B5%E5%B7%AE-%E5%BB%A3%E6%9D%B1%E8%A9%B1/966481932?i=966481945&l=en"
+    }
 ];
